1cc5ae2942c702eff804411d386beda69ee9db20
Glen Rogers <wisco.barbell@gmail.com>
2025-08-12 11:00:37 -0500

    Update App and index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import NutritionApp from './EnhancedApp.jsx';
+import MealSwipeApp from './App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<NutritionApp />);
+root.render(<MealSwipeApp />);
